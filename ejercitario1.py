260c2a2c0ba1010dfff0314cf233bb562ca09991
AlexPalma-cyber <alexinfiny@gmail.com>
2024-08-09 16:18:59 -0400

    Add files via upload

---
+++ ejercitario1.py
@@ -0,0 +1,3 @@
+x= int(input("ingrese un el primer numero"))
+y= int(input("ingrese un el segundo numero"))
+print("La suma de los numeros es:" , x / y )
---
+++ ejercitario4.py
@@ -0,0 +1,4 @@
+motocicleta = int(input("cantidad de kilometros recorridos"))
+combustible = int(input("cantidad de litros consumidos"))
+resultado = combustible/motocicleta
+print(resultado)
