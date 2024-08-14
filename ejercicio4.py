f2c1e89529d222e13e0795396aae28587f592535
AlexPalma-cyber <alexinfiny@gmail.com>
2024-08-14 16:25:33 -0400

    Add files via upload

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,17 +1,40 @@
-valores = []
+import random
+
 while True:
-    try:
-        valor = int(input("Ingrese los numeros: "))
-        if valor == 0:
-            break
-        if numero_primo(valor):
-            valores.append(valor)
-    except ValueError:
-        print("Inserta un numero valido")
+    aleatorio = random.randrange(0,3)
+    elijePc = ""
+    print("1. Piedra")
+    print("2. Papel")
+    print("3. Tijera")
+    opcion = int(input("Elige tu opcion"))
 
+    if opcion == 1:
+        elijeUsuario = "Piedra"
+    elif opcion == 2:
+        elijeUsuario = "Papel"
+    elif opcion == 3:
+        elijeUsuario = "Tijera"
+    print("Elejiste: ", elijeUsuario)
 
-if valores:
-    print("Los numeros primos son: ", valor)
-else:
-    print("Ningun numero ingresado es un numero primo")
-
+    if aleatorio == 0:
+        elijePc = "Piedra"
+    elif aleatorio == 1:
+        elijePc = "Papel"
+    elif aleatorio == 2:
+        elijePc = "Tijera"
+    print("La maquina elijio: ", elijePc)
+    print("...")
+    if elijePc == "Piedra" and elijeUsuario == "Papel":
+        print("Ganaste, papel envuelve Piedra")   
+    elif elijePc == "Papel" and elijeUsuario == "Tijera":
+        print("Ganaste, Tijera corta papel") 
+    elif elijePc == "Tijera" and elijeUsuario == "Piedra":
+        print("Ganaste, Piedra machaca Tijera") 
+    if elijePc == "Papel" and elijeUsuario == "Piedra":
+        print("Perdiste, papel envuelve Piedra") 
+    elif elijePc == "Tijera" and elijeUsuario == "Papel":
+        print("Perdiste, Tijera corta papel") 
+    elif elijePc == "Piedra" and elijeUsuario == "Tijera":
+        print("Perdiste, Piedra machaca Tijera")
+    elif elijePc == elijeUsuario:
+        print("empate")       
