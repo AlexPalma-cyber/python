c8f51ea2db8fedcdd3826f177cd59ab35f792e2c
AlexPalma-cyber <alexinfiny@gmail.com>
2024-09-10 14:33:29 -0400

    Add files via upload

---
+++ php_Alex/calculadora.php
@@ -0,0 +1,19 @@
+
+
+<input name="n1" id="n1">
+<input name="n2" id="n2">
+<input name="r" id="r">
+
+<button id="sumar">
+    Sumar</button>
+<button id="restar">
+    Restar</button>
+<button id="multiplicar">
+    multiplicar</button>
+<button id="dividir">
+    Dividir</button>
+<button id="elevar">
+    Elevar</button>   
+<script src="scripts.js">
+
+</script>    
