626d18666a0fa3dd4eeb70fbc9c9de88360d05eb
AlexPalma-cyber <alexinfiny@gmail.com>
2024-09-17 16:23:42 -0400

    Add files via upload

---
+++ php_Alex/clase6/BD/conexion BD/conexion.php
@@ -0,0 +1,6 @@
+<?php
+    include 'vars.php';
+    include 'https://www.data.com/file.php';
+
+    $mysql = new mysql($host,$user ,$password, $database, $port);
+?>
---
+++ php_Alex/clase6/BD/conexion BD/vars.php
@@ -0,0 +1,7 @@
+<?php
+   $host = "localhost";
+   $user = "admin";
+   $password = "12345";
+   $database = "bd_base";
+   $port = 3306;
+?>
