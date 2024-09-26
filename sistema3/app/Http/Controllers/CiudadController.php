83053b0a5183140e2a08fb010d7fdc9cf1563525
AlexPalma-cyber <alexinfiny@gmail.com>
2024-09-26 13:16:51 -0400

    Add files via upload

---
+++ sistema3/app/Http/Controllers/CiudadController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ciudad;
+use Illuminate\Http\Request;
+
+class CiudadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $data['ciudades'] = Ciudad::paginate(5);
+        return view('ciudad.index')->with($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //guardar en la tabla de ciudads
+        $ciudad =new ciudad ();
+        $ciudad->nombre= $request->city;
+        $ciudad->save();
+        return redirect('/ciudad');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ciudad $ciudad)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Ciudad $ciudad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Ciudad $ciudad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ciudad $ciudad)
+    {
+        //
+    }
+}
