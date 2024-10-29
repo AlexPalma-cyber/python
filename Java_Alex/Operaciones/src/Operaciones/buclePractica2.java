879e7c89191731c8ee21f6a5425872f04774ceca
AlexPalma-cyber <alexinfiny@gmail.com>
2024-10-29 13:36:25 -0300

    Add files via upload

--- Java_Alex/Operaciones/src/Operaciones/buclePractica2.java
+++ Java_Alex/Operaciones/src/Operaciones/buclePractica2.java
@@ -8,8 +8,8 @@ public class buclePractica2 {
         for(inicio = 0; inicio < limite; inicio++) {
             System.out.println("Imprimo un " + inicio);
         }
-
-        int cat1, cat2, hip;
+        
+        int cat1, cat2, hip, contador = 0;
 
         System.out.println("-> c² = a² + b² ");
         for (hip = 1; hip <= 1000; hip++) {//hipotenusa
@@ -18,9 +18,11 @@ public class buclePractica2 {
                     //condicional
                     if (hip == Math.sqrt(cat1*cat1+cat2*cat2)) {
                         System.out.println("a. " + hip + "² = " + cat1 + "² + " + cat2 + "²");
+                        contador++;
                     }
                 }
             }
         }
+        System.out.println("Cantidad" + contador);
     }
 }
